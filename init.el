9e6b217bd4758d0ff847e7ddfa3c27fb86e6e990
Alexandre Adolphe <alexandre.adolphe@gmail.com>
2017-09-03 02:36:11 +0200

    Police par défaut en DéjàVu sans mono.

--- init.el
+++ init.el
@@ -2,11 +2,6 @@
 
 (prefer-coding-system 'utf-8-unix)
 
-;; Setting default face
-(set-face-attribute 'default nil
-     :foreground "white"
-     :background "black")
-
 (eval-and-compile
   ;; Windows : change modifiers
   (when (string= system-type "windows-nt")
@@ -76,6 +71,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "PfEd" :family "DejaVu Sans Mono"))))
  '(cursor ((t (:background "red")))))
 
 ;; Variables pour retirer les avertissements sur certaines fonctions
