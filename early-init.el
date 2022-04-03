676f92c271b5554fbd7996e83cf56fe28e6474c6
Alexandre Adolphe <alexandre.adolphe@gmail.com>
2022-04-03 15:02:55 +0200

    Augmente le gc-threshold pour diminuer le temps de démarrage.

--- early-init.el
+++ early-init.el
@@ -1,5 +1,7 @@
 (setq package-enable-at-startup nil)
+(setq gc-cons-threshold most-positive-fixnum)
 
 (add-hook 'emacs-startup-hook
           (lambda ()
+            (setq gc-cons-threshold 800000)
             (message "init time %s" (emacs-init-time))))
