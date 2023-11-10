cd8c759999058981879ebd18a6551e5e2b52cb51
Alexandre Adolphe <alexandre.adolphe@gmail.com>
2023-11-10 01:14:06 +0100

    Fix du dossier de backup.

--- init.el
+++ init.el
@@ -63,7 +63,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(backup-directory-alist (quote ((".*" . (format "%s/%s" user-emacs-directory "backups")))))
+ '(backup-directory-alist `((".*" . ,(format "%s/%s" user-emacs-directory "backups"))))
  '(blink-cursor-mode nil)
  '(delete-selection-mode t)
  '(dired-listing-switches "-alh")
